1be8d1ccb98d278edbe72152b9b0dcbf6bfa4fd0
myesmin <myesmin415@gmail.com>
2026-04-26 00:58:32 -0400

    day03 excercises

--- Week01/day03/main.cpp
+++ Week01/day03/main.cpp
@@ -1,10 +1,120 @@
 #include <iostream>
-
+/*
 int main() {
     int age = 20;
 
     if (age >= 18) {
         std::cout << "You are an adult" << "\n";
+    }
+    return 0;
+}
+*/
+
+/*
+int main() {
+    int age = 15;
+
+    if (age >= 18) { 
+        std::cout << "You are an adult" << "\n"; // runs if condition is TRUE
+    } else {
+        std::cout << "You are not an adult" << "\n"; // runs if condition is FALSE
+    }
+    return 0;
+}
+
+int main() {
+    int score = 75;
+
+    if (score >= 90) {
+        std::cout << "Grade: A" << "\n";
+    } else if (score >= 80) {
+        std::cout << "Grade: B" << "\n";
+    } else if (score >= 70) {
+        std::cout << "Grade: C" << "\n"; 
+    } else {
+        std::cout << "Grade: F" << "\n";
+    }
+    return 0;
+}
+
+
+int main() {
+    double accuracy = 0.75;
+
+    if (accuracy >= 0.95) {
+        std::cout << "Model is excellent" << "\n";
+    } else if (accuracy >= 0.80) {
+        std::cout << "Model is good" << "\n";
+    } else if (accuracy >= 0.70) {
+        std::cout << "Model needs imorovement" << "\n";
+    } else {
+        std::cout << "Model is poor" << "\n";
+    }
+    return 0;
+}
+
+
+int main() {
+    int x = 10;
+
+    if (x != 10) {
+        std::cout << "x is not 10" << "\n";
+    } else {
+        std::cout << "x is 10" << "\n";
+    }
+    return 0;
+}
+
+
+int main() {
+    int age = 20;
+    bool hasID = true;
+
+    if (age >=18 && hasID) {
+        std::cout << "You can enter" << "\n";
+    } else {
+        std::cout << "You cannot enter" << "\n";
+    }
+    return 0;
+}
+
+
+int main() {
+    int age = 15;
+    bool hasPermission = true;
+
+    if (age >= 18 || hasPermission) {
+        std::cout << "You can enter" << "\n";
+    } else {
+        std::cout << "You cannot enter" << "\n";
+    }
+    return 0;
+}
+
+
+int main() {
+    bool isRaining = false;
+
+    if (!isRaining) {
+        std::cout << "Go Outside!" << "\n";
+    } else {
+        std::cout << "Stay Inside!" << "\n";
+    }
+    return 0;
+}
+
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << i << "\n";
+    }
+    return 0;
+}
+*/
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        std::cout << i * 2 << "\n";
     }
     return 0;
 }
